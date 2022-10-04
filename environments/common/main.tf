974ba9b9d187fe90920e6f26fde74c90e8a3b669
Risheak Chauhan <risheak@popup.store>
2022-10-04 11:03:50 +0530

    authenticate to k8s apis using access token

--- environments/common/main.tf
+++ environments/common/main.tf
@@ -1,10 +1,7 @@
 data "google_client_config" "default" {}
 provider "kubernetes" {
   host = module.k8s.endpoint
-  username = module.k8s.username
-  password = module.k8s.password
-  client_certificate = base64decode(module.k8s.client_certificate)
-  client_key = base64decode(module.k8s.client_key)
+  token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.k8s.cluster_ca_certificate)
 }
 
